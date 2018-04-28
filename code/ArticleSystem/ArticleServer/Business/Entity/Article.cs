c50bf426a2ba0e28d9c73f30fbaa2a6ac926d787
Ács Dávid <acsdavid97@gmail.com>
2018-04-28 23:35:24 +0300

    project data access started, project structure defined

---
+++ code/ArticleSystem/ArticleServer/Business/Entity/Article.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArticleServer.Business.Entity
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Abstract { get; set; }
+        public WriterUser Writer { get; set; }
+        public string Body { get; set; }
+    }
+}
