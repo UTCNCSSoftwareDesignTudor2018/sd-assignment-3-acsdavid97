53c37d279333f4af5e54e4d80ebd55b620f8b607
Ács Dávid <acsdavid97@gmail.com>
2018-05-02 14:58:40 +0300

    GUI interface started

---
+++ code/ArticleServer/ArticleGUIClient/ArticleClient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using ArticleClientServerCommons;
+using ArticleClientServerCommons.Dto;
+
+namespace ArticleGUIClient
+{
+    public class ArticleClient
+    {
+        private readonly NetworkStream _stream;
+
+        public ArticleClient()
+        {
+            var client = new TcpClient("127.0.0.1", 11000);
+            _stream = client.GetStream();
+        }
+
+        public IList<ArticleDto> GetArticleDtos()
+        {
+            Utils.SendObject(Constants.GetArticlesCommand, _stream);
+            var articles = Utils.ReadObject<ArticleDto[]>(_stream);
+            return articles;
+        }
+    }
+}
---
+++ code/ArticleServer/ArticleGUIClient/ArticleViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using ArticleClientServerCommons.Dto;
+using ArticleGUIClient.Annotations;
+
+namespace ArticleGUIClient
+{
+    public class ArticleViewModel : INotifyPropertyChanged
+    {
+        private readonly ArticleClient _client = new ArticleClient();
+        private ArticleDto _selectedArticleDto;
+
+        public ArticleViewModel()
+        {
+            Articles = _client.GetArticleDtos();
+        }
+
+        public IList<ArticleDto> Articles { get; private set; }
+
+        public ICommand UpdateCommand => new RelayCommand<object>(ExecuteUpdate, CanExecuteUpdate);
+
+        private static bool CanExecuteUpdate(object o)
+        {
+            return true;
+        }
+
+        private void ExecuteUpdate(object o)
+        {
+            Articles = _client.GetArticleDtos();
+            OnPropertyChanged(nameof(Articles));
+        }
+
+        public ArticleDto SelectedArticleDto
+        {
+            get => _selectedArticleDto;
+            set
+            {
+                _selectedArticleDto = value; 
+                OnPropertyChanged(nameof(SelectedArticleDto));
+            } 
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
